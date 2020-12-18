f10c9267c8d8b15e26ee9625926cbb5570e20e98
Diego <alvarodiego@gmail.com>
2020-12-18 09:24:59 -0300

    PessoaFisica criada e revisão na publicidade de Pessoa

---
+++ src/Entities/PessoaFisica.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace src.Entities
+{
+    class PessoaFisica : Pessoa
+    {
+        public double GastoSaude { get; private set; }
+
+        public PessoaFisica()
+        {
+
+        }
+
+        public PessoaFisica(string nome, double rendaAnual, double gasttoSaude)
+            : base(nome, rendaAnual)
+        {
+            GastoSaude = GastoSaude;
+        }
+
+        public override double Imposto()
+        {
+            double aliquota = 0;
+            if (RendaAnual < 20000.0)
+            {
+                aliquota = 0.15;
+            }
+            else
+            {
+                aliquota = 0.25;
+            }
+
+            return aliquota * RendaAnual - 0.5 * GastoSaude;
+        }
+    }
+}
